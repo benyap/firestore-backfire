649e43153734787a3620a0913a3028aefa27b9bf
Ben Yap <contact@benyap.com>
2021-12-28 11:13:24 +1100

    chore: add linting and testing frameworks

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  ignorePatterns: ["cjs", "esm"],
+  rules: {},
+};
